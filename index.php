cee4a57d3614fbd16179f624c2e564ce4297dda6
Dimaaan1233 <122044889+Dimaaan1233@users.noreply.github.com>
2023-01-05 22:33:31 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,73 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Безымянная страница</title>
+<meta name="generator" content="WYSIWYG Web Builder 17 - https://www.wysiwygwebbuilder.com">
+<link href="Безымянный1.css" rel="stylesheet">
+<link href="index.css" rel="stylesheet">
+</head>
+<body>
+<div id="container">
+<div id="wb_EmbeddedPage1" style="position:absolute;left:5px;top:0px;width:1235px;height:1080px;z-index:20;">
+<div id="Layer1" style="position:absolute;text-align:center;left:19px;top:8px;width:1166px;height:169px;z-index:0;">
+<div id="Layer1_Container" style="width:1160px;position:relative;margin-left:auto;margin-right:auto;text-align:left;">
+</div>
+</div>
+<div id="wb_Image1" style="position:absolute;left:60px;top:192px;width:468px;height:60px;z-index:1;">
+<img src="images/468.gif" id="Image1" alt="" width="468" height="60"></div>
+<div id="wb_Image2" style="position:absolute;left:677px;top:192px;width:468px;height:60px;z-index:2;">
+<img src="images/468.gif" id="Image2" alt="" width="468" height="60"></div>
+<div id="wb_Shape1" style="position:absolute;left:19px;top:266px;width:207px;height:371px;z-index:3;">
+<img src="images/img0001.png" id="Shape1" alt="" width="207" height="371" style="width:207px;height:371px;"></div>
+<div id="NavigationBar2" style="position:absolute;left:27px;top:317px;width:190px;height:310px;z-index:4;">
+<ul class="navbar">
+<li><a href="./index.php"><img src="images/img0002_over.png" class="hover"><span><img src="images/img0002.png"></span></a></li>
+<li><a href="./emul.html"><img src="images/img0003_over.png" class="hover"><span><img src="images/img0003.png"></span></a></li>
+<li><a href=""><img src="images/img0004_over.png" class="hover"><span><img src="images/img0004.png"></span></a></li>
+<li><a href=""><img src="images/img0005_over.png" class="hover"><span><img src="images/img0005.png"></span></a></li>
+<li><a href=""><img src="images/img0006_over.png" class="hover"><span><img src="images/img0006.png"></span></a></li>
+<li><a href=""><img src="images/img0008_over.png" class="hover"><span><img src="images/img0008.png"></span></a></li>
+<li><a href=""><img src="images/img0009_over.png" class="hover"><span><img src="images/img0009.png"></span></a></li>
+<li><a href=""><img src="images/img0010_over.png" class="hover"><span><img src="images/img0010.png"></span></a></li>
+<li><a href=""><img src="images/img0011_over.png" class="hover"><span><img src="images/img0011.png"></span></a></li>
+<li><a href=""><img src="images/img0012_over.png" class="hover"><span><img src="images/img0012.png"></span></a></li>
+</ul>
+</div>
+<div id="wb_Shape3" style="position:absolute;left:19px;top:275px;width:207px;height:33px;z-index:5;">
+<img src="images/img0013.png" id="Shape3" alt="" width="207" height="33" style="width:207px;height:33px;"></div>
+<div id="wb_Shape2" style="position:absolute;left:19px;top:268px;width:207px;height:33px;z-index:6;">
+<img src="images/img0014.png" id="Shape2" alt="" width="207" height="33" style="width:207px;height:33px;"></div>
+<div id="wb_Text2" style="position:absolute;left:86px;top:271px;width:90px;height:32px;z-index:7;">
+<span style="color:#DCDCDC;font-family:Arial;font-size:27px;"><strong>Меню</strong></span></div>
+<div id="wb_Image5" style="position:absolute;left:19px;top:646px;width:200px;height:300px;z-index:8;">
+<img src="images/200.gif" id="Image5" alt="" width="200" height="300"></div>
+<div id="wb_Shape4" style="position:absolute;left:998px;top:268px;width:206px;height:496px;z-index:9;">
+<img src="images/img0015.png" id="Shape4" alt="" width="206" height="496" style="width:206px;height:496px;"></div>
+<div id="wb_Shape5" style="position:absolute;left:998px;top:266px;width:206px;height:35px;z-index:10;">
+<img src="images/img0016.png" id="Shape5" alt="" width="206" height="35" style="width:206px;height:35px;"></div>
+<div id="wb_Shape6" style="position:absolute;left:999px;top:273px;width:206px;height:35px;z-index:11;">
+<img src="images/img0017.png" id="Shape6" alt="" width="206" height="35" style="width:206px;height:35px;"></div>
+<div id="wb_Text3" style="position:absolute;left:1062px;top:269px;width:142px;height:29px;z-index:12;">
+<span style="color:#DCDCDC;font-family:Arial;font-size:24px;"><strong>Топ игр</strong></span></div>
+<div id="wb_Image3" style="position:absolute;left:1017px;top:315px;width:165px;height:116px;z-index:13;">
+<img src="images/comix-zone_220.jpg" id="Image3" alt="" width="165" height="115"></div>
+<div id="wb_Image6" style="position:absolute;left:1018px;top:469px;width:165px;height:116px;z-index:14;">
+<img src="images/battletoads-double-dragon-the-ultimate-team_220.jpg" id="Image6" alt="" width="165" height="115"></div>
+<div id="wb_Image7" style="position:absolute;left:1017px;top:615px;width:165px;height:116px;z-index:15;">
+<img src="images/sonic-the-hedgehog_220.jpg" id="Image7" alt="" width="165" height="115"></div>
+<div id="wb_Text4" style="position:absolute;left:1064px;top:435px;width:117px;height:19px;z-index:16;">
+<span style="color:#000000;font-family:Tahoma;font-size:16px;">Comix zone</span></div>
+<div id="wb_Text5" style="position:absolute;left:1009px;top:589px;width:226px;height:19px;z-index:17;">
+<span style="color:#000000;font-family:Tahoma;font-size:16px;">Battletoads double dragon</span></div>
+<div id="wb_Text6" style="position:absolute;left:1077px;top:735px;width:49px;height:19px;z-index:18;">
+<span style="color:#000000;font-family:Tahoma;font-size:16px;">Sonic</span></div>
+<div id="wb_Image4" style="position:absolute;left:999px;top:774px;width:200px;height:300px;z-index:19;">
+<img src="images/200.gif" id="Image4" alt="" width="200" height="300"></div>
+</div>
+<img src="images/img0007.png" id="MergedObject2" alt="" title="" style="border-width:0;position:absolute;left:237px;top:266px;width:736px;height:580px;z-index:21" width="736" height="580">
+<div id="wb_Text1" style="position:absolute;left:540px;top:270px;width:142px;height:29px;z-index:22;">
+<span style="color:#DCDCDC;font-family:Arial;font-size:24px;"><strong>Главная</strong></span></div>
+</div>
+</body>
+</html>
